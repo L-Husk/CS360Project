4bf1cdd9b610a6ec80151798f70b3e8b9b09e7b2
L-Husk <lhusk04@gmail.com>
2025-03-22 19:37:26 -0700

    basicprog

--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Question
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_questions = Question.objects.order_by("-pub_date")[:5]
+    template = loader.get_template("listings/index.html")
+    context = {
+        "latest_questions": latest_questions
+    }
+    return HttpResponse(template.render(context, request))

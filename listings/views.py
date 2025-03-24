4e9a375469f804c7ee420669bef7b0190f15c009
L-Husk <lhusk04@gmail.com>
2025-03-24 14:38:49 -0700

    bugfix

--- listings/views.py
+++ listings/views.py
@@ -3,15 +3,8 @@ from django.http import HttpResponse
 from django.template import loader
 from .models import Listing
 
-from .models import Question
-# Create your views here.
-"""def index(request):
-    latest_questions = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("listings/index.html")
-    context = {"latest_questions": latest_questions}
-    return HttpResponse(template.render(context, request))
-"""
 
+# Create your views here.
 def index(request):
     latest_posts = Listing.objects.order_by("-pub_date")
     template = loader.get_template("listings/index.html")

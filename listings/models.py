9d32c9a7bc0a24e7d19369eaa3809c8dc14295a4
HunterB03 <huntsam03@icloud.com>
2025-03-24 18:34:36 -0700

    images

--- listings/models.py
+++ listings/models.py
@@ -1,8 +1,13 @@
 from django.db import models
+from django.conf import settings
+ 
+User = settings.AUTH_USER_MODEL
 
 # Create your models here.
 class Listing(models.Model):
     item_name = models.CharField(max_length=100)
     item_desc = models.CharField(max_length=500)
     pub_date = models.DateTimeField("date published")
-    img_link = models.CharField(max_length=100)
+    img = models.ImageField(default='default.jpg', blank=True)
+    user = models.ForeignKey(User, default=1, null=True,on_delete=models.SET_NULL)
+
--- mysite/urls.py
+++ mysite/urls.py
@@ -16,11 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path("listings/", include("listings.urls")),
     path("users/", include("users.urls")),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) 
 
 admin.site.site_url = '/listings/'
